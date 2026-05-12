cec0513254b7f728a6c290330836859874fda49d
syoch <syoch64@gmail.com>
2026-05-12 17:38:12 +0900

    some

--- components/droid/p30t/default.nix
+++ components/droid/p30t/default.nix
@@ -5,10 +5,12 @@
 {
   system.stateVersion = "24.05";
 
-  environment.packages = with pkgs; [
-    neovim
-    hostname
+  environment.packages = [
+    pkgs.neovim
+    pkgs.hostname
+    pkgs.zsh
   ];
+  user.shell = "${pkgs.zsh}/bin/zsh";
 
   environment.etcBackupExtension = ".bak";
   services.sshd.enable = true;
